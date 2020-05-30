04d2e0259038343bb7ec0e7284892f00e6b24cf1
Klas Reimer <Klas-Reimer@gitbub.com>
2020-05-30 14:51:36 +0200

    Dateien hinzugefügt

---
+++ RFIDconfig.py
@@ -0,0 +1,15 @@
+uidcard1 = [248, 132, 30, 74, 40]
+name1 = "Blue Chip1"
+content1 ="Hunde"
+#link1 = "https://www.youtube.com/"
+link1 = "https://www.youtube.com/watch?v=pu9sQ8rTIwA"
+
+uidcard2 = [137, 225, 47, 164, 227] 
+name2 = "Blue Chip2"
+content2 = "Katzen"
+link2 = "https://www.youtube.com/watch?v=pu9sQ8rTIwA"
+
+uidcard3 = [193, 101, 118, 30, 204]
+name3 = "White Card1"
+content3 = "Peppa Pig"
+link3 = "https://www.youtube.com/watch?v=pu9sQ8rTIwA"
